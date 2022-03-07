1855480209e9968e2a729415481efafd566c5b7c
fahra akhlaq <fahraakhlaq@fahras-mbp.home>
2022-03-07 14:19:15 +0000

    form components added using mui and routre link for signup added

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,7 +1,22 @@
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
+import Link from '@mui/material/Link';
+import {Link as RouterLink} from 'react-router-dom'
+
+
 
 
 export const LoginForm = () =>{
-    return  <Button variant="contained">Hello World</Button>;
+    return  (
+        <Box component="form" sx={{marginTop:8, display:"flex", flexDirection: "column", alignItems: "center"}}>
+            <TextField margin="normal" id="email" label="Email" variant="outlined" name="email" autoFocus fullWidth/>
+            <TextField margin="normal" id="password" label="Password" variant="outlined" name="password" type="password" autoFocus fullWidth/>
+            <Button type="submit" variant="contained" sx={{mt:3, mb:2}}>Login</Button>
+      <Link component={RouterLink} variant="body2" to="/sign-up" underline='none'>
+        Dont have an account? Signup
+      </Link>
+        </Box>
+    )
 
 }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,9 @@
 import { LoginForm } from "../components/LoginForm"
+import Container from '@mui/material/Container';
+
 
 export const Login = () =>{
-    return <div>
+    return <Container component="main" maxWidth="xs">
         <LoginForm/>
-    </div>
+    </Container>
 }
