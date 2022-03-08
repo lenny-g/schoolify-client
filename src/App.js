670085da73845ef817d21b6f5ca282faaa60e274
fahra akhlaq <fahraakhlaq@fahras-mbp.home>
2022-03-08 20:03:34 +0000

    added apollo

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import { AppRouter } from './components/AppRouter';
+import {
+	ApolloClient,
+	InMemoryCache,
+	ApolloProvider,
+} from '@apollo/client';
+
+const client = new ApolloClient({
+	uri: '"http://localhost:4000/"',
+	cache: new InMemoryCache(),
+});
 
 function App() {
-  return (
-<>
-<BrowserRouter>
-<AppRouter/>
-</BrowserRouter>
-</>
-  );
+	return (
+			<ApolloProvider client={client}>
+				<BrowserRouter>
+					<AppRouter />
+				</BrowserRouter>
+			</ApolloProvider>
+	);
 }
 
 export default App;
