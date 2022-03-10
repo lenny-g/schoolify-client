c769d286d0d474f8a08710e1a281f027848b4435
lenny-g <leanne_gallag@live.co.uk>
2022-03-10 22:58:46 +0000

    added basic nav bar

--- src/components/NavigationBar/PublicNavBar.js
+++ src/components/NavigationBar/PublicNavBar.js
@@ -1,55 +1,173 @@
-import React from "react";
-import {
-  AppBar,
-  Toolbar,
-  CssBaseline,
-  Typography,
-  makeStyles,
-} from "@material-ui/core";
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import MuiDrawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import CssBaseline from "@mui/material/CssBaseline";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import ListItem from "@mui/material/ListItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import LoginIcon from "@mui/icons-material/Login";
+import Link from "@mui/material/Link";
+import InfoIcon from "@mui/icons-material/Info";
 
-const useStyles = makeStyles((theme) => ({
-  navlinks: {
-    marginLeft: theme.spacing(10),
-    display: "flex",
+import AssignmentIndIcon from "@mui/icons-material/AssignmentInd";
+
+import HomeIcon from "@mui/icons-material/Home";
+import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
+import { Login } from "@mui/icons-material";
+
+const drawerWidth = 240;
+
+const openedMixin = (theme) => ({
+  width: drawerWidth,
+  transition: theme.transitions.create("width", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.enteringScreen,
+  }),
+  overflowX: "hidden",
+});
+
+const closedMixin = (theme) => ({
+  transition: theme.transitions.create("width", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  overflowX: "hidden",
+  width: `calc(${theme.spacing(7)} + 1px)`,
+  [theme.breakpoints.up("sm")]: {
+    width: `calc(${theme.spacing(8)} + 1px)`,
   },
-  logo: {
-    flexGrow: "1",
-    cursor: "pointer",
-  },
-  link: {
-    textDecoration: "none",
-    color: "white",
-    fontSize: "20px",
-    marginLeft: theme.spacing(20),
-    "&:hover": {
-      color: "blue",
-    },
-  },
+});
+
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "flex-end",
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+}));
+
+const Drawer = styled(MuiDrawer, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  width: drawerWidth,
+  flexShrink: 0,
+  whiteSpace: "nowrap",
+  boxSizing: "border-box",
+  ...(open && {
+    ...openedMixin(theme),
+    "& .MuiDrawer-paper": openedMixin(theme),
+  }),
+  ...(!open && {
+    ...closedMixin(theme),
+    "& .MuiDrawer-paper": closedMixin(theme),
+  }),
 }));
 
 export const PublicNavBar = () => {
-  const classes = useStyles();
+  const [open, setOpen] = useState(false);
+
+  const handleDrawerToggle = () => {
+    setOpen(!open);
+  };
 
   return (
-    <AppBar position="static">
+    <Box sx={{ display: "flex" }}>
       <CssBaseline />
-      <Toolbar>
-        <Typography variant="h4" className={classes.logo}>
-          SCHOOLIFY
-        </Typography>
-        <div className={classes.navlinks}>
-          <Link to="/login" className={classes.link}>
-            Login
-          </Link>
-          <Link to="/sign-up" className={classes.link}>
-            Sign Up
-          </Link>
-          <Link to="/contact" className={classes.link}>
-            Contact
-          </Link>
-        </div>
-      </Toolbar>
-    </AppBar>
+      <Drawer variant="permanent" open={open}>
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerToggle}>
+            {!open ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        <List
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            height: "100%",
+            margin: "2rem 0rem",
+          }}
+        >
+          <ListItem button component={Link} href="/home">
+            <ListItemIcon
+              sx={{
+                color: "#979DAC",
+              }}
+            >
+              <HomeIcon />
+            </ListItemIcon>
+            <ListItemText
+              disableTypography={true}
+              sx={{
+                color: "#979DAC",
+                fontFamily: "Arial",
+              }}
+              primary="Home"
+            />
+          </ListItem>
+          <Divider />
+          <ListItem button component={Link} href="/login">
+            <ListItemIcon
+              sx={{
+                color: "#979DAC",
+              }}
+            >
+              <LoginIcon />
+            </ListItemIcon>
+            <ListItemText
+              disableTypography={true}
+              sx={{
+                color: "#979DAC",
+                fontFamily: "Arial",
+              }}
+              primary="Login"
+            />
+          </ListItem>
+          <ListItem button component={Link} href="/sign-up">
+            <ListItemIcon
+              sx={{
+                color: "#979DAC",
+              }}
+            >
+              <AssignmentIndIcon />
+            </ListItemIcon>
+            <ListItemText
+              disableTypography={true}
+              sx={{
+                color: "#979DAC",
+                fontFamily: "Arial",
+              }}
+              primary="Sign Up"
+            />
+          </ListItem>
+          <Divider />
+          <ListItem button component={Link} href="/about-us">
+            <ListItemIcon
+              sx={{
+                color: "#979DAC",
+              }}
+            >
+              <InfoIcon />
+            </ListItemIcon>
+            <ListItemText
+              disableTypography={true}
+              sx={{
+                color: "#979DAC",
+                fontFamily: "Arial",
+              }}
+              primary="About us"
+            />
+          </ListItem>
+        </List>
+      </Drawer>
+    </Box>
   );
 };
