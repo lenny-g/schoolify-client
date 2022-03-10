611200512050171a0f677c09e2750b958cadebce
lenny-g <leanne_gallag@live.co.uk>
2022-03-10 21:39:04 +0000

    added basic navbar

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,13 +1,23 @@
-import { LoginForm } from "../components/LoginForm/LoginForm"
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
+import { LoginForm } from "../components/LoginForm/LoginForm";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import { PublicNavBar } from "../components/NavigationBar/PublicNavBar";
 
-
-export const Login = () =>{
-    return <Container component="main" maxWidth="xs">
-         <Typography variant="h3" gutterBottom component="div" sx={{textAlign:"center"}}>
-        Login
-      </Typography>
-        <LoginForm/>
+export const Login = () => {
+  return (
+    <Container>
+      <PublicNavBar />
+      <Container component="main" maxWidth="xs">
+        <Typography
+          variant="h3"
+          gutterBottom
+          component="div"
+          sx={{ textAlign: "center" }}
+        >
+          Login
+        </Typography>
+        <LoginForm />
+      </Container>
     </Container>
-}
+  );
+};
