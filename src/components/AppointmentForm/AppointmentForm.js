88076c7b93de443fddcff954612228fc394ba73a
fahra akhlaq <fahraakhlaq@fahras-mbp.home>
2022-03-10 19:15:59 +0000

    react hook form issue for appointment drop down

--- src/components/AppointmentForm/AppointmentForm.js
+++ src/components/AppointmentForm/AppointmentForm.js
@@ -8,7 +8,7 @@ import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
-import { useForm } from 'react-hook-form';
+import { useForm, Controller } from 'react-hook-form';
 import DateTimePicker from '@mui/lab/DateTimePicker';
 
 const appointmentOptions = ['Medical', 'Dental', 'Other'];
@@ -22,6 +22,7 @@ export const AppointmentForm = () => {
 		setValue,
 		getValues,
 		handleSubmit,
+		control
 	} = useForm();
 
 	const value = getValues('enrollDate');
@@ -34,12 +35,19 @@ export const AppointmentForm = () => {
 		setAbsenceDate(value || null);
 	}, [setAbsenceDate, value]);
 
+	const [appointmentType, setAppointmentType] = useState('');
+
+	const handleAppointmentType = (event) => {
+		console.log(event.currentTarget.defaultValue, 'cheking appointment type');
+		setAppointmentType(event.target.value);
+		console.log(appointmentType);
+	};
+
 	const onSubmit = async (data) => {
 		console.log(data);
 	};
 
 	return (
-
 		<LocalizationProvider dateAdapter={AdapterDateFns}>
 			<Box component='form' onSubmit={handleSubmit(onSubmit)}>
 				<Box
@@ -51,13 +59,33 @@ export const AppointmentForm = () => {
 					}}>
 					<FormControl sx={{ width: 420 }}>
 						<InputLabel id='appointmentType'>Absence type</InputLabel>
-						<Select
+						<Controller
+							name='select'
+							control={control}
+							render={({ field }) => (
+								<Select
+									{...field}
+									options={[
+										{ value: 'chocolate', label: 'Chocolate' },
+										{ value: 'strawberry', label: 'Strawberry' },
+										{ value: 'vanilla', label: 'Vanilla' },
+									]}
+								/>
+							)}
+						/>
+
+						{/* <Select
 							labelId='appointmentType'
 							id='appointmentType'
 							label='Appointment type'
-							defaultValue='Medical'
-							{...register('appointmentType')}
-							error={!!errors.appointmentType}>
+							defaultValue={appointmentType}
+							onChange={(event) => {
+								setAppointmentType(event.target.value);
+								console.log(event.target.defaultValue);
+							}}
+							// {...register('appointmentType')}
+							// error={!!errors.appointmentType}
+						>
 							{appointmentOptions.map((title, index) => {
 								return (
 									<MenuItem key={index} value={title}>
@@ -65,26 +93,28 @@ export const AppointmentForm = () => {
 									</MenuItem>
 								);
 							})}
-						</Select>
+						</Select> */}
 					</FormControl>
 				</Box>
-				<Box
-					sx={{
-						margin: 2,
-						padding: 2,
-						alignItems: 'center',
-						backgroundColor: 'pink',
-					}}>
-					<TextField
-						id='description'
-						label='Description'
-						multiline
-						fullWidth
-						rows={4}
-						{...register('description', { required: true })}
-						error={!!errors.description}
-					/>
-				</Box>
+				{appointmentType === 'Other' && (
+					<Box
+						sx={{
+							margin: 2,
+							padding: 2,
+							alignItems: 'center',
+							backgroundColor: 'pink',
+						}}>
+						<TextField
+							id='description'
+							label='Description'
+							multiline
+							fullWidth
+							rows={4}
+							{...register('description', { required: true })}
+							error={!!errors.description}
+						/>
+					</Box>
+				)}
 				<DateTimePicker
 					label='Please select date and time'
 					inputFormat='MM/dd/yyyy hh:mm'
