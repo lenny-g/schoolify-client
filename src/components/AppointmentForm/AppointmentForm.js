1fd60de681adadeca0104dfdb0e48dbd15f4846f
fahra akhlaq <fahraakhlaq@fahras-mbp.home>
2022-03-10 15:40:22 +0000

    wip

--- src/components/AppointmentForm/AppointmentForm.js
+++ src/components/AppointmentForm/AppointmentForm.js
@@ -1,154 +1,126 @@
-import * as React from 'react';
+import React, { useEffect, useState } from 'react';
 import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import CalendarPicker from '@mui/lab/CalendarPicker';
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
-import TimePicker from '@mui/lab/TimePicker';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
-import { useForm } from "react-hook-form";
+import { useForm } from 'react-hook-form';
+import DateTimePicker from '@mui/lab/DateTimePicker';
 
-const appointmentOptions = ['medical', 'dental', 'other'];
+const appointmentOptions = ['Medical', 'Dental', 'Other'];
 
 export const AppointmentForm = () => {
-	const [date, setDate] = React.useState(new Date());
-	const [value, setValue] = React.useState(null);
+	const [absenceDate, setAbsenceDate] = useState();
 
-    const {
-        register,
-        formState: { errors },
-        handleSubmit,
-      } = useForm();
+	const {
+		register,
+		formState: { errors },
+		setValue,
+		getValues,
+		handleSubmit,
+	} = useForm();
+
+	const value = getValues('enrollDate');
+
+	useEffect(() => {
+		register('dateOfAbsence');
+	}, [register]);
+
+	useEffect(() => {
+		setAbsenceDate(value || null);
+	}, [setAbsenceDate, value]);
 
 	const onSubmit = async (data) => {
 		console.log(data);
 	};
 
 	return (
+
 		<LocalizationProvider dateAdapter={AdapterDateFns}>
-			<Typography
-				variant='subtitle2'
-				gutterBottom
-				component='div'
-				sx={{ mt: 2, textAlign: 'center' }}>
-				Please select appointment type:
-			</Typography>
-
-			<Box
-				component='form'
-				sx={{
-					margin: 2,
-					padding: 2,
-					alignItems: 'center',
-					backgroundColor: 'pink',
-				}}>
-				<FormControl sx={{ width: 420 }}>
-					<InputLabel id='title'>Title</InputLabel>
-					<Select labelId='title' id='title' label='Title' defaultValue='medical' >
-						{appointmentOptions.map((title, index) => {
-							return (
-								<MenuItem key={index} value={title}>
-									{title}
-								</MenuItem>
-							);
-						})}
-					</Select>
-				</FormControl>
-			</Box>
-			<Box
-				component='form'
-				sx={{
-					margin: 2,
-					padding: 2,
-					alignItems: 'center',
-					backgroundColor: 'pink',
-				}}>
-				<TextField
-					id='other'
-					label='Other'
-					multiline
-					fullWidth
-					rows={2}
-					defaultValue='Default Value'
-                    {...register("other", { required: true })}
-        error={!!errors.password}
-				/>
-			</Box>
-			<Typography
-				variant='subtitle2'
-				gutterBottom
-				component='div'
-				sx={{ mt: 2, textAlign: 'center' }}>
-				Appointment History:
-			</Typography>
-			<Box
-				component='form'
-				sx={{
-					margin: 2,
-					padding: 2,
-					alignItems: 'center',
-					backgroundColor: 'lightBlue',
-				}}>
-				<Typography
-					variant='subtitle2'
-					gutterBottom
-					component='div'
-					sx={{ mt: 2 }}>
-					Appointment Status:
-				</Typography>
-			</Box>
-			<Box
-				component='form'
-				sx={{
-					margin: 2,
-					padding: 2,
-					alignItems: 'center',
-					backgroundColor: 'orange',
-				}}>
-				<Typography
-					variant='subtitle2'
-					gutterBottom
-					component='div'
-					sx={{ mt: 2 }}>
-					Appointment history:
-				</Typography>
-			</Box>
-
-			<CalendarPicker date={date} onChange={(newDate) => setDate(newDate)} />
-
-			<Box
-				component='form'
-				sx={{
-					margin: 2,
-					padding: 2,
-					alignItems: 'center',
-					backgroundColor: 'yellow',
-				}}>
-				<TimePicker
-					label='Appointment Time'
-					value={value}
-					onChange={(newValue) => {
-						setValue(newValue);
+			<Box component='form' onSubmit={handleSubmit(onSubmit)}>
+				<Box
+					sx={{
+						margin: 2,
+						padding: 2,
+						alignItems: 'center',
+						backgroundColor: 'pink',
+					}}>
+					<FormControl sx={{ width: 420 }}>
+						<InputLabel id='appointmentType'>Absence type</InputLabel>
+						<Select
+							labelId='appointmentType'
+							id='appointmentType'
+							label='Appointment type'
+							defaultValue='Medical'
+							{...register('appointmentType')}
+							error={!!errors.appointmentType}>
+							{appointmentOptions.map((title, index) => {
+								return (
+									<MenuItem key={index} value={title}>
+										{title}
+									</MenuItem>
+								);
+							})}
+						</Select>
+					</FormControl>
+				</Box>
+				<Box
+					sx={{
+						margin: 2,
+						padding: 2,
+						alignItems: 'center',
+						backgroundColor: 'pink',
+					}}>
+					<TextField
+						id='description'
+						label='Description'
+						multiline
+						fullWidth
+						rows={4}
+						{...register('description', { required: true })}
+						error={!!errors.description}
+					/>
+				</Box>
+				<DateTimePicker
+					label='Please select date and time'
+					inputFormat='MM/dd/yyyy hh:mm'
+					value={absenceDate}
+					onChange={(value) => {
+						setAbsenceDate(value);
+						setValue('dateOfAbsence', value, {
+							shouldValidate: true,
+							shouldDirty: true,
+						});
 					}}
-					renderInput={(params) => <TextField {...params} />}
+					renderInput={(params) => (
+						<TextField
+							{...params}
+							{...register('dateOfAbsence', { required: true })}
+							error={!!errors.dateOfAbsence}
+							margin='normal'
+							id='dateOfAbsence'
+							variant='outlined'
+							name='dateOfAbsence'
+							fullWidth
+							error={!!errors.dateOfAbsence}
+						/>
+					)}
 				/>
-			</Box>
-			<Box
-				component='form'
-				sx={{
-					margin: 2,
-					padding: 2,
-					alignItems: 'center',
-					backgroundColor: 'red',
-				}}>
-				<Button variant='contained' f>
-					Request
-				</Button>
+				<Box
+					sx={{
+						margin: 2,
+						padding: 2,
+						alignItems: 'center',
+						backgroundColor: 'red',
+					}}>
+					<Button variant='contained' type='submit'>
+						Request
+					</Button>
+				</Box>
 			</Box>
 		</LocalizationProvider>
 	);
