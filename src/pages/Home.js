53fb27b848f815ca826567f8178d78026faeabe5
lenny-g <leanne_gallag@live.co.uk>
2022-03-11 16:41:50 +0000

    added logo to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,14 @@
 import Container from "@mui/material/Container";
 import { PublicNavBar } from "../components/NavigationBar/PublicNavBar";
+import schoolify from "../assets/img/logo.png";
 
 export const Home = () => {
   return (
     <Container component="main" maxWidth="xs">
       <PublicNavBar />
-      <h1>Home</h1>
+      <Container sx={{ alignItems: "center", mt: "7rem" }}>
+        <img src={schoolify} />
+      </Container>
     </Container>
   );
 };
