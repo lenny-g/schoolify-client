d3ecff7a73580749af914ff425bd6c2383a000ea
lenny-g <leanne_gallag@live.co.uk>
2022-03-11 14:35:15 +0000

    Creating team member card

--- src/components/TeamMemberCard/TeamMemberCard.js
+++ src/components/TeamMemberCard/TeamMemberCard.js
@@ -3,7 +3,6 @@ import { styled } from "@mui/material/styles";
 import Card from "@mui/material/Card";
 import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
-import CardActions from "@mui/material/CardActions";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import GitHubIcon from "@mui/icons-material/GitHub";
