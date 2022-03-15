5b277110bd27fc6dad6f7694fe3e9a6f0dd8e8bc
fahra akhlaq <fahraakhlaq@fahras-mbp.home>
2022-03-15 16:15:35 +0000

    added styling to medical form

--- src/pages/Medical.js
+++ src/pages/Medical.js
@@ -1,13 +1,23 @@
 import { MedicalForm } from '../components/MedicalForm';
 import Container from '@mui/material/Container';
 import { PublicNavBar } from '../components/NavigationBar/PublicNavBar';
+import { Paper } from '@material-ui/core';
+
+const styles = {
+	paperContainer: {
+		margin: '2rem 0',
+		borderRadius: '25px',
+	},
+};
 
 export const Medical = () => {
 	return (
 		<Container>
 			<PublicNavBar />
-			<Container component='main' maxWidth='xs'>
-				<MedicalForm />
+			<Container component='main' maxWidth='md'>
+				<Paper elevation={6} style={styles.paperContainer}>
+					<MedicalForm />
+				</Paper>
 			</Container>
 		</Container>
 	);
