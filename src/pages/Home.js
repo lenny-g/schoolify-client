4bfd522c28e7cccfbc07916cc810355d48823f93
fahra akhlaq <fahraakhlaq@fahras-mbp.home>
2022-03-15 15:53:04 +0000

    completed content to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,7 @@ import { HomePageNavBtn } from '../components/HomePageNavBtn';
 import { Paper } from '@material-ui/core';
 import logoMain from '../assets/img/logoMain.png';
 import { colors } from '../styles';
+import { HomePageIntro } from '../components/HomePageIntro';
 
 const styles = {
 	paperContainer: {
@@ -21,21 +22,7 @@ export const Home = () => {
 					<div className='logoContainer'>
 						<img src={logoMain} className='logo' />
 					</div>
-					{/* <Container
-						sx={{
-							display: 'flex',
-							flexDirection: 'row',
-							flexWrap: 'wrap',
-							justifyContent: 'center',
-							m: '5rem',
-						}}>
-						<h1>Welcome the Parents signup/login portal.</h1>
-						<p>
-							Please sign-up and then login to be transfered to your dashboard
-							where you can keep up to date with your child's progress and
-							register any appointments
-						</p>
-					</Container> */}
+					<HomePageIntro />
 					<HomePageNavBtn />
 				</Paper>
 			</Container>
