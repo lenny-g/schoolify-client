3cfd05f2bf3cb030e752e74bd11eba955d04f046
fahra akhlaq <fahraakhlaq@fahras-mbp.home>
2022-03-17 20:46:33 +0000

    added react responsive

--- src/components/NavigationBar/AppNavigationBar.js
+++ src/components/NavigationBar/AppNavigationBar.js
@@ -1,30 +1,28 @@
+import React from 'react';
+import { useMediaQuery } from 'react-responsive';
 import { useState, useEffect } from 'react';
 import { SideNavbar } from './SideNavbar';
 import { TopNavbar } from './TopNavbar';
 
 const userType = JSON.parse(localStorage.getItem('user'))?.role;
 
-const UserNavBar = () => {
-	const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+// useEffect(() => {
+//   window.addEventListener("resize", () => {
+//     setWindowWidth(window.innerWidth);
+//   });
 
-	// useEffect(() => {
-	//   window.addEventListener("resize", () => {
-	//     setWindowWidth(window.innerWidth);
-	//   });
-
-	//   return () => {
-	//     window.removeEventListener("resize", () => {
-	//       setWindowWidth(window.innerWidth);
-	//     });
-	//   };
-	// }, []);
-};
+//   return () => {
+//     window.removeEventListener("resize", () => {
+//       setWindowWidth(window.innerWidth);
+//     });
+//   };
+// }, []);
 
 export const AppNavigationBar = () => {
-	return (
-		<>
-			{/* <SideNavbar /> */}
-			<TopNavbar />
-		</>
-	);
+	const isDesktopOrLaptop = useMediaQuery({
+		query: '(min-width: 1224px)',
+	});
+	const isDesktop = useMediaQuery({ query: '(min-width: 551px)' });
+	const isMobile = useMediaQuery({ query: '(max-width: 550px)' });
+	return <>{isDesktop ? <SideNavbar /> : <TopNavbar />}</>;
 };
