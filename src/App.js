1affe89495825143e24bc639b4fdf62eaedb9a7b
fahra akhlaq <fahraakhlaq@fahras-mbp.home>
2022-03-17 13:43:51 +0000

    added new navbar

--- src/App.js
+++ src/App.js
@@ -1,51 +1,57 @@
 import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  createHttpLink,
-} from "@apollo/client";
+	ApolloClient,
+	InMemoryCache,
+	ApolloProvider,
+	createHttpLink,
+} from '@apollo/client';
 
-import { setContext } from "@apollo/client/link/context";
+import { setContext } from '@apollo/client/link/context';
 
-import { BrowserRouter } from "react-router-dom";
-import "./App.css";
+import { BrowserRouter } from 'react-router-dom';
+import './App.css';
 
-import { AppRouter } from "./components/AppRouter";
-import { AppNavigationBar } from "./components/NavigationBar/AppNavigationBar";
-import { AppProvider } from "./context/AppProvider";
+import { AppRouter } from './components/AppRouter';
+import { AppNavigationBar } from './components/NavigationBar/AppNavigationBar';
+import { AppProvider } from './context/AppProvider';
+
+import Box from '@mui/material/Box';
 
 const link = createHttpLink({
-  uri: "http://localhost:4000/",
-  credentials: "same-origin",
+	uri: 'http://localhost:4000/',
+	credentials: 'same-origin',
 });
 
 const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem("token");
+	const token = localStorage.getItem('token');
 
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : "",
-    },
-  };
+	return {
+		headers: {
+			...headers,
+			authorization: token ? `Bearer ${token}` : '',
+		},
+	};
 });
 
 const client = new ApolloClient({
-  link: authLink.concat(link),
-  cache: new InMemoryCache(),
+	link: authLink.concat(link),
+	cache: new InMemoryCache(),
 });
 
 function App() {
-  return (
-    <ApolloProvider client={client}>
-      <AppProvider>
-        <AppNavigationBar />
-        <BrowserRouter>
-          <AppRouter />
-        </BrowserRouter>
-      </AppProvider>
-    </ApolloProvider>
-  );
+	return (
+		<ApolloProvider client={client}>
+			<AppProvider>
+				<Box sx={{ display: 'flex' }}>
+					<AppNavigationBar />
+					<BrowserRouter>
+						<Box component='main' sx={{ flexGrow: 1, p: 3 }}>
+							<AppRouter />
+						</Box>
+					</BrowserRouter>
+				</Box>
+			</AppProvider>
+		</ApolloProvider>
+	);
 }
 
 export default App;
--- src/components/NavigationBar/AppNavigationBar.js
+++ src/components/NavigationBar/AppNavigationBar.js
@@ -1,42 +1,34 @@
-import { useState, useEffect } from "react";
-import { ParentNavBar } from "./ParentNavBar";
-import { TeacherNavBar } from "./TeacherNavBar";
-import { PublicNavBar } from "./PublicNavBar";
-import { MobileParentNavBar } from "./MobileParentNavBar";
-import { MobilePublicNavBar } from "./MobilePublicNavBar";
-import { MobileTeacherNavBar } from "./MobileTeacherNavBar";
+import { useState, useEffect } from 'react';
+import { ParentNavBar } from './ParentNavBar';
+import { TeacherNavBar } from './TeacherNavBar';
+import { PublicNavBar } from './PublicNavBar';
+import { MobileParentNavBar } from './MobileParentNavBar';
+import { MobilePublicNavBar } from './MobilePublicNavBar';
+import { MobileTeacherNavBar } from './MobileTeacherNavBar';
+import { SideNavbar } from './SideNavbar';
 
-const userType = JSON.parse(localStorage.getItem("user"))?.role;
+const userType = JSON.parse(localStorage.getItem('user'))?.role;
 
 const UserNavBar = () => {
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+	const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
-  useEffect(() => {
-    window.addEventListener("resize", () => {
-      setWindowWidth(window.innerWidth);
-    });
+	// useEffect(() => {
+	//   window.addEventListener("resize", () => {
+	//     setWindowWidth(window.innerWidth);
+	//   });
 
-    return () => {
-      window.removeEventListener("resize", () => {
-        setWindowWidth(window.innerWidth);
-      });
-    };
-  }, []);
-
-  if (userType === "teacher") {
-    return windowWidth > 800 ? <TeacherNavBar /> : <MobileTeacherNavBar />;
-  }
-  if (userType === "parent") {
-    return windowWidth > 800 ? <ParentNavBar /> : <MobileParentNavBar />;
-  } else {
-    return windowWidth > 800 ? <PublicNavBar /> : <MobilePublicNavBar />;
-  }
+	//   return () => {
+	//     window.removeEventListener("resize", () => {
+	//       setWindowWidth(window.innerWidth);
+	//     });
+	//   };
+	// }, []);
 };
 
 export const AppNavigationBar = () => {
-  return (
-    <div>
-      <UserNavBar />
-    </div>
-  );
+	return (
+		<>
+			<SideNavbar />
+		</>
+	);
 };
