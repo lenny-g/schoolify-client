2b694f496c91eae67e9cc48230bb3ed041855d82
fahra akhlaq <fahraakhlaq@fahras-mbp.home>
2022-03-19 00:13:50 +0000

    wip

--- src/components/IncidentChannel/index.js
+++ src/components/IncidentChannel/index.js
@@ -24,7 +24,14 @@ export const IncidentChannel = () => {
 				</Grid>
 				<Grid container>
 					<Grid item xs={12} sx={{ display: 'flex', justifyContent: 'end' }}>
-						<Box sx={item.teacherComment}> hello mum</Box>
+						<Box sx={item.teacherComment}>
+							<Typography variant='body1'>
+								Billy hurt his knee today after getting into a fight with a
+								nother student. however this has been dealth with, and we can
+								assure you taht this wont happen agaoin.
+							</Typography>
+							<Typography variant='caption'>Miss Jones</Typography>
+						</Box>
 					</Grid>
 					<Grid
 						item
@@ -33,7 +40,13 @@ export const IncidentChannel = () => {
 							display: 'flex',
 							justifyContent: 'start',
 						}}>
-						<Box sx={item.parentComment}> hi miss</Box>
+						<Box sx={item.parentComment}>
+							{' '}
+							<Typography variant='body1'>
+								thanks for letting me know
+							</Typography>
+							<Typography variant='caption'>10.30am</Typography>
+						</Box>
 					</Grid>
 					<Grid
 						item
@@ -42,10 +55,17 @@ export const IncidentChannel = () => {
 							display: 'flex',
 							justifyContent: 'start',
 						}}>
-						<Box sx={item.parentComment}> hows my son today?</Box>
+						<Box sx={item.parentComment}>
+							<Typography variant='body1'>i will have a word later</Typography>
+							<Typography variant='caption'>10.40am</Typography>
+						</Box>
 					</Grid>
 					<Grid item xs={12} sx={{ display: 'flex', justifyContent: 'end' }}>
-						<Box sx={item.teacherComment}> slight tummy pain- but hes ok!</Box>
+						<Box sx={item.teacherComment}>
+							{' '}
+							<Typography variant='body1'>great!</Typography>
+							<Typography variant='caption'>10.40am</Typography>
+						</Box>
 					</Grid>
 				</Grid>
 			</Box>
