92eb16ba1a1604ab651547595c408777978672ce
Luiz Carlos Froes <luizfroes@gmail.com>
2022-03-22 20:26:50 +0000

    render the absence summary

--- src/components/ChildDashboard/AbsenceRequestSummary/index.js
+++ src/components/ChildDashboard/AbsenceRequestSummary/index.js
@@ -1,54 +1,91 @@
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Accordion from '@mui/material/Accordion';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import AccordionDetails from '@mui/material/AccordionDetails';
+import parseISO from "date-fns/parseISO";
 
-const stylingRowColor = (status) => {
-	if (status === 'PENDING') return 'lightGray';
-	if (status === 'APPROVED') return 'lightGreen';
-	if (status === 'REJECTED') return 'red';
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import Stack from "@mui/material/Stack";
+import Alert from "@mui/material/Alert";
+
+const stylingAbsenceRequestColor = (status) => {
+  if (status === "PENDING") return "#ead885";
+  if (status === "APPROVED") return "#79d8a5";
+  if (status === "REJECTED") return "#ef8080";
 };
 
-const checkStatus = (absenceRequest) => {
-	if (absenceRequest.status === 'reject') {
-	}
-	return true;
+const checkStatus = (status) => {
+  if (status !== "REJECTED") {
+  }
+  return "disabled";
 };
 
 export const AbsenceRequestSummary = ({ childData }) => {
-	console.log(childData);
-	return (
-		<Box>
-			<Typography
-				className='headingFont'
-				variant='subtitle1'
-				gutterBottom
-				component='Box'>
-				Absence Requests:
-			</Typography>
-			{childData?.absenceRequests?.map((absenceRequest, index) => {
-				return (
-					<Accordion
-						disabled
-						sx={{ backgroundColor: stylingRowColor(absenceRequest.status) }}>
-						<AccordionSummary>
-							<Typography>
-								{absenceRequest.type} {absenceRequest.dateTime}
-								{absenceRequest.status}
-							</Typography>
-						</AccordionSummary>
-						{checkStatus(absenceRequest) && (
-							<AccordionDetails>
-								<Typography>
-									We cannot allow child to have a day off for a family trip, as
-									there are ample opportunities during half term break.
-								</Typography>
-							</AccordionDetails>
-						)}
-					</Accordion>
-				);
-			})}
-		</Box>
-	);
+  const formattedChildData = childData?.absenceRequests?.map(
+    (absenceRequest) => {
+      return {
+        absenceRequestId: absenceRequest.id,
+        type: absenceRequest.type,
+        description: absenceRequest.description,
+        dateTime: `${
+          parseISO(absenceRequest.dateTime).toGMTString().split("GMT")[0]
+        }  `,
+        status: absenceRequest.status,
+      };
+    }
+  );
+
+  console.log(checkStatus("PENDING"));
+
+  return (
+    <Box width="100%">
+      <Typography className="headingFont" variant="subtitle1" gutterBottom>
+        Absence Requests:
+      </Typography>
+      {formattedChildData.length === 0 && (
+        <Alert severity="info">
+          You have made no absence requests yet, click on the 'request absence'
+          button to submit one.
+        </Alert>
+      )}
+      {formattedChildData?.map((absenceRequest, index) => {
+        return (
+          <Accordion
+            key={index}
+            disabled
+            sx={{
+              backgroundColor: stylingAbsenceRequestColor(
+                absenceRequest.status
+              ),
+              mb: "10px",
+            }}
+          >
+            <AccordionSummary
+              sx={{
+                backgroundColor: stylingAbsenceRequestColor(
+                  absenceRequest.status
+                ),
+              }}
+            >
+              <Stack sx={{ width: "100%" }}>
+                <Typography align="center">{absenceRequest.type}</Typography>
+                <Typography align="center">
+                  {absenceRequest.dateTime}
+                </Typography>
+                <Typography align="center">{absenceRequest.status}</Typography>
+              </Stack>
+            </AccordionSummary>
+            {absenceRequest.status === "REJECTED" && (
+              <AccordionDetails>
+                <Typography>
+                  We cannot allow child to have a day off for a family trip, as
+                  there are ample opportunities during half term break.
+                </Typography>
+              </AccordionDetails>
+            )}
+          </Accordion>
+        );
+      })}
+    </Box>
+  );
 };
--- src/pages/StudentInfo.js
+++ src/pages/StudentInfo.js
@@ -1,65 +1,70 @@
-import LinearProgress from '@mui/material/LinearProgress';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
-import { PageContainer } from '../components/PageContainer/';
+import LinearProgress from "@mui/material/LinearProgress";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import { PageContainer } from "../components/PageContainer/";
 
-import { VIEW_CHILD } from '../graphql/query';
+import { VIEW_CHILD } from "../graphql/query";
 
-import { useParams } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
-import { item, forms, GREEN, headers } from '../styles';
-import { AbsenceRequestSummary } from '../components/ChildDashboard/AbsenceRequestSummary';
-import { ChildProfileCard } from '../components/ChildDashboard/ChildProfileCard';
-import { PageTitle } from '../components/PageTitle';
+import { useParams } from "react-router-dom";
+import { useQuery } from "@apollo/client";
+import { item, forms, GREEN, headers } from "../styles";
+import { AbsenceRequestSummary } from "../components/ChildDashboard/AbsenceRequestSummary";
+import { ChildProfileCard } from "../components/ChildDashboard/ChildProfileCard";
+import { PageTitle } from "../components/PageTitle";
 
 export const StudentInfo = (props) => {
-	const { studentId } = useParams();
+  const { studentId } = useParams();
 
-	const { data, loading, error, refetch } = useQuery(VIEW_CHILD, {
-		variables: { studentId },
+  const { data, loading, error, refetch } = useQuery(VIEW_CHILD, {
+    variables: { studentId },
 
-		pollInterval: 1000,
-	});
-	const childData = data?.viewChild;
-	if (error) {
-		return <div>ERROR</div>;
-	}
-	if (loading) {
-		return <LinearProgress style={{ backgroundColor: 'purple' }} />;
-	}
+    pollInterval: 1000,
+  });
+  const childData = data?.viewChild;
+  if (error) {
+    return <div>ERROR</div>;
+  }
+  if (loading) {
+    return <LinearProgress style={{ backgroundColor: "purple" }} />;
+  }
 
-	console.log(childData);
-	console.log(data.viewChild);
+  console.log(data.viewChild);
 
-	return (
-		<PageContainer>
-			<Grid container sx={item.outerContainer}>
-				<Grid item xs={12}>
-					<PageTitle>
-						{childData.firstName} {childData.lastName}'s Dashboard
-					</PageTitle>
-				</Grid>
-				<Grid container>
-					<Grid item xs={12} md={4}>
-						<Box sx={{ ...forms.container, backgroundColor: GREEN }}>
-							<ChildProfileCard childData={childData} />
-						</Box>
-					</Grid>
-					<Grid item xs={12} md={4}>
-						<Box sx={{ ...forms.container, backgroundColor: GREEN }}>Box1</Box>
-						<Box sx={{ ...forms.container, backgroundColor: GREEN }}>Box2</Box>
-					</Grid>
+  return (
+    <PageContainer>
+      <Grid container sx={item.outerContainer}>
+        <Grid item xs={12}>
+          <PageTitle>
+            {childData.firstName} {childData.lastName}'s Dashboard
+          </PageTitle>
+        </Grid>
+        <Grid container>
+          <Grid item xs={12} md={4}>
+            <Box sx={{ ...forms.container, backgroundColor: GREEN }}>
+              <ChildProfileCard childData={childData} />
+            </Box>
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <Box sx={{ ...forms.container, backgroundColor: GREEN }}>
+              Container1
+            </Box>
+            <Box sx={{ ...forms.container, backgroundColor: GREEN }}>
+              Container2
+            </Box>
+          </Grid>
 
-					<Grid item xs={12} md={4}>
-						<Box sx={{ ...forms.container, backgroundColor: GREEN }}>
-							<AbsenceRequestSummary childData={childData} />
-						</Box>
+          <Grid item xs={12} md={4}>
+            <Box sx={{ ...forms.container, backgroundColor: GREEN }}>
+              <AbsenceRequestSummary childData={childData} />
+            </Box>
 
-						<Box sx={{ ...forms.container, backgroundColor: GREEN }}>Box4</Box>
-					</Grid>
-				</Grid>
-			</Grid>
-		</PageContainer>
-	);
+            <Box sx={{ ...forms.container, backgroundColor: GREEN }}>
+              Container4
+            </Box>
+          </Grid>
+        </Grid>
+      </Grid>
+    </PageContainer>
+  );
 };
