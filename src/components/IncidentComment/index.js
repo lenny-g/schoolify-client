cdd86b1fd2f15f0ae2713072eed47d708bc0a4d7
Luiz Carlos Froes <luizfroes@gmail.com>
2022-03-23 17:09:39 +0000

    refactor

--- src/components/IncidentComment/index.js
+++ src/components/IncidentComment/index.js
@@ -1,75 +1,84 @@
-import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
-import TextField from '@mui/material/TextField';
-import { useForm } from 'react-hook-form';
-import LoadingButton from '@mui/lab/LoadingButton';
-import { item, forms, GREEN } from '../../styles';
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+import { useForm } from "react-hook-form";
+import LoadingButton from "@mui/lab/LoadingButton";
+import { item, forms, GREEN } from "../../styles";
+import { useState } from "react";
 
 export const IncidentComment = ({
-	mutationError,
-	executeAddComment,
-	incidentReportDataById,
-	showCommentSection,
+  mutationError,
+  executeAddComment,
+  incidentReportDataById,
+  showCommentSection,
 
-	refetch,
+  refetch,
 }) => {
-	const {
-		register,
-		formState: { errors },
-		handleSubmit,
-	} = useForm();
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
 
-	const onSubmit = async (data) => {
-		await executeAddComment({
-			variables: {
-				input: {
-					incidentReportId: incidentReportDataById.id,
-					name: JSON.parse(localStorage.getItem('user')).firstName,
-					message: data.comment,
-				},
-			},
-		});
+  const [commentValue, setCommentValue] = useState("");
 
-		refetch();
-	};
+  console.log(commentValue);
 
-	return (
-		<Grid container component='form' onSubmit={handleSubmit(onSubmit)}>
-			<Grid sx={{ ...forms.container, backgroundColor: GREEN }} xs={12}>
-				<TextField
-					color='warning'
-					margin='normal'
-					id='comment'
-					label='Comment'
-					variant='outlined'
-					name='comment'
-					rows={4}
-					multiline
-					fullWidth
-					{...register('comment', { required: true })}
-					error={!!errors.comment}
-				/>
-			</Grid>
-			<Grid sx={item.comment} xs={12}>
-				<LoadingButton
-					// loading={mutationLoading}
-					color='warning'
-					disabled={!showCommentSection}
-					type='submit'
-					variant='contained'
-					sx={forms.loadingButton}>
-					Submit
-				</LoadingButton>
-				{!!mutationError && (
-					<Typography
-						variant='subtitle2'
-						gutterBottom
-						component='div'
-						sx={forms.errorContainer}>
-						Failed to add comment.
-					</Typography>
-				)}
-			</Grid>
-		</Grid>
-	);
+  const onSubmit = async (data) => {
+    await executeAddComment({
+      variables: {
+        input: {
+          incidentReportId: incidentReportDataById.id,
+          name: JSON.parse(localStorage.getItem("user")).firstName,
+          message: data.comment,
+        },
+      },
+    });
+
+    refetch();
+  };
+
+  return (
+    <Grid container component="form" onSubmit={handleSubmit(onSubmit)}>
+      <Grid sx={{ ...forms.container, backgroundColor: GREEN }} xs={12}>
+        <TextField
+          color="warning"
+          margin="normal"
+          id="comment"
+          label="Comment"
+          variant="outlined"
+          name="comment"
+          rows={4}
+          multiline
+          fullWidth
+          {...register("comment", { required: true })}
+          error={!!errors.comment}
+        >
+          {commentValue}
+        </TextField>
+      </Grid>
+      <Grid sx={item.comment} xs={12}>
+        <LoadingButton
+          // loading={mutationLoading}
+          color="warning"
+          disabled={!showCommentSection}
+          type="submit"
+          variant="contained"
+          sx={forms.loadingButton}
+        >
+          Submit
+        </LoadingButton>
+        {!!mutationError && (
+          <Typography
+            variant="subtitle2"
+            gutterBottom
+            component="div"
+            sx={forms.errorContainer}
+          >
+            Failed to add comment.
+          </Typography>
+        )}
+      </Grid>
+    </Grid>
+  );
 };
