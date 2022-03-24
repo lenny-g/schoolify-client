eecb8f089b564838e515f6622514e47d54cbb9c8
Luiz Carlos Froes <luizfroes@gmail.com>
2022-03-24 23:30:12 +0000

    Added confetti

--- src/components/ChildDashboard/AbsenceRequestSummary/index.js
+++ src/components/ChildDashboard/AbsenceRequestSummary/index.js
@@ -1,121 +1,132 @@
-import parseISO from 'date-fns/parseISO';
-import { useNavigate } from 'react-router-dom';
-import { useAuth } from '../../../context/AppProvider';
+import parseISO from "date-fns/parseISO";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../../context/AppProvider";
 
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Accordion from '@mui/material/Accordion';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import AccordionDetails from '@mui/material/AccordionDetails';
-import Stack from '@mui/material/Stack';
-import Alert from '@mui/material/Alert';
-import Button from '@mui/material/Button';
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import Stack from "@mui/material/Stack";
+import Alert from "@mui/material/Alert";
+import Button from "@mui/material/Button";
 
 const stylingAbsenceRequestColor = (status) => {
-	if (status === 'PENDING') return '#ead885';
-	if (status === 'APPROVED') return '#79d8a5';
-	if (status === 'REJECTED') return '#ef8080';
+  if (status === "PENDING") return "#ead885";
+  if (status === "APPROVED") return "#79d8a5";
+  if (status === "REJECTED") return "#ef8080";
 };
 
 const checkStatus = (status) => {
-	if (status !== 'REJECTED') {
-	}
-	return true;
+  if (status !== "REJECTED") {
+  }
+  return true;
 };
 
 export const AbsenceRequestSummary = ({ childData }) => {
-	const navigate = useNavigate();
+  const navigate = useNavigate();
 
-	const { user } = useAuth();
+  const { user } = useAuth();
 
-	const isParent = () => {
-		if (user.role === 'parent') {
-			return true;
-		}
-	};
+  const isParent = () => {
+    if (user.role === "parent") {
+      return true;
+    }
+  };
 
-	const formattedChildData = childData?.absenceRequests?.map(
-		(absenceRequest) => {
-			return {
-				absenceRequestId: absenceRequest.id,
-				type: absenceRequest.type,
-				description: absenceRequest.description,
-				dateTime: `${
-					parseISO(absenceRequest.dateTime).toGMTString().split('GMT')[0]
-				}  `,
-				status: absenceRequest.status,
-			};
-		}
-	);
+  const formattedChildData = childData?.absenceRequests?.map(
+    (absenceRequest) => {
+      return {
+        absenceRequestId: absenceRequest.id,
+        type: absenceRequest.type,
+        description: absenceRequest.description,
+        dateTime:
+          parseISO(absenceRequest.dateTime).toGMTString().split("GMT")[0] ===
+          "Invalid Date"
+            ? absenceRequest.dateTime
+            : parseISO(absenceRequest.dateTime).toGMTString().split("GMT")[0],
+        status: absenceRequest.status,
+      };
+    }
+  );
 
-	return (
-		<Stack width="100%">
-			<Typography className="headingFont" variant="subtitle1" align="center">
-				Absence Requests:
-			</Typography>
-			<Typography variant="caption" gutterBottom align="center">
-				Click to view status
-			</Typography>
-			{formattedChildData.length === 0 && (
-				<>
-					{isParent === false ? (
-						<Alert variant="outlined" severity="info">
-							{childData.firstName} {childData.lastName} has no absence requests
-							yet.
-						</Alert>
-					) : (
-						<>
-							<Alert variant="outlined" severity="info">
-								{childData.firstName} {childData.lastName} has no absence
-								requests yet, click on the 'Request Absence' button to submit
-								one.
-							</Alert>
-							<Button
-								sx={{ mt: 2, width: '100%' }}
-								variant="contained"
-								color="warning"
-								size="small"
-								onClick={() => {
-									navigate('/absenceRequest/new', { replace: true });
-								}}>
-								Request Absence
-							</Button>
-						</>
-					)}
-				</>
-			)}
-			{formattedChildData?.map((absenceRequest, index) => {
-				return (
-					<Box
-						disabled={checkStatus(absenceRequest.status)}
-						key={index}
-						sx={{
-							borderRadius: '10px',
-							backgroundColor: stylingAbsenceRequestColor(
-								absenceRequest.status
-							),
-							mb: '10px',
-						}}>
-						<Stack
-							sx={{
-								width: '100%',
-								padding: '5px 0px',
-								borderRadius: '10px',
-								backgroundColor: stylingAbsenceRequestColor(
-									absenceRequest.status
-								),
-							}}>
-							<Typography align="center">{absenceRequest.status}</Typography>
-							<Typography variant="subtitle1" align="center">
-								{absenceRequest.dateTime}
-							</Typography>
-							<Typography variant="caption" align="center">
-								{absenceRequest.type}
-							</Typography>
-						</Stack>
-					</Box>
-				);
-			})}
-		</Stack>
-	);
+  return (
+    <Stack width="100%">
+      <Typography className="headingFont" variant="subtitle1" align="center">
+        Absence Requests:
+      </Typography>
+      <Typography variant="caption" gutterBottom align="center">
+        Click to view status
+      </Typography>
+      {formattedChildData.length === 0 && (
+        <>
+          {isParent === false ? (
+            <Alert variant="outlined" severity="info">
+              {childData.firstName} {childData.lastName} has no absence requests
+              yet.
+            </Alert>
+          ) : (
+            <>
+              <Alert variant="outlined" severity="info">
+                {childData.firstName} {childData.lastName} has no absence
+                requests yet, click on the 'Request Absence' button to submit
+                one.
+              </Alert>
+              <Button
+                sx={{ mt: 2, width: "100%" }}
+                variant="contained"
+                color="warning"
+                size="small"
+                onClick={() => {
+                  navigate("/absenceRequest/new", { replace: true });
+                }}
+              >
+                Request Absence
+              </Button>
+            </>
+          )}
+        </>
+      )}
+      {formattedChildData?.map((absenceRequest, index) => {
+        return (
+          <Box
+            onClick={() => {
+              navigate("/absenceRequest/view", { replace: true });
+            }}
+            disabled={checkStatus(absenceRequest.status)}
+            key={index}
+            sx={{
+              borderRadius: "10px",
+              backgroundColor: stylingAbsenceRequestColor(
+                absenceRequest.status
+              ),
+              mb: "10px",
+              "&:hover": {
+                border: "2px solid #979dac",
+              },
+            }}
+          >
+            <Stack
+              sx={{
+                width: "100%",
+                padding: "5px 0px",
+                borderRadius: "10px",
+                backgroundColor: stylingAbsenceRequestColor(
+                  absenceRequest.status
+                ),
+              }}
+            >
+              <Typography align="center">{absenceRequest.status}</Typography>
+              <Typography variant="subtitle1" align="center">
+                {absenceRequest.dateTime}
+              </Typography>
+              <Typography variant="caption" align="center">
+                {absenceRequest.type}
+              </Typography>
+            </Stack>
+          </Box>
+        );
+      })}
+    </Stack>
+  );
 };
--- src/components/ChildDashboard/ChildCertificates/index.js
+++ src/components/ChildDashboard/ChildCertificates/index.js
@@ -1,129 +1,180 @@
-import Stack from '@mui/material/Stack';
-import Typography from '@mui/material/Typography';
-import Alert from '@mui/material/Alert';
-import Box from '@mui/material/Box';
-import Modal from '@mui/material/Modal';
-import Button from '@mui/material/Button';
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
+import Alert from "@mui/material/Alert";
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
+import Button from "@mui/material/Button";
 
-import { useNavigate } from 'react-router-dom';
-import { useMediaQuery } from 'react-responsive';
-import { ChildCertificateCard } from './ChildCertificateCard';
-import { useState } from 'react';
-import { modal } from '../../../styles';
-import { MOBILE } from '../../../media';
-import { useAuth } from '../../../context/AppProvider';
+import confetti from "canvas-confetti";
+import { useNavigate } from "react-router-dom";
+import { useMediaQuery } from "react-responsive";
+import { ChildCertificateCard } from "./ChildCertificateCard";
+import { useState } from "react";
+import { modal } from "../../../styles";
+import { MOBILE } from "../../../media";
+import { useAuth } from "../../../context/AppProvider";
 
 export const ChildCertificates = ({ childData, certificateImg }) => {
-	const [open, setOpen] = useState(false);
+  const count = 500;
+  const defaults = {
+    origin: { y: 0.7 },
+    zIndex: 10000,
+  };
 
-	const handleOpen = () => {
-		setOpen(true);
-	};
+  const fire = (particleRatio, opts) => {
+    confetti(
+      Object.assign({}, defaults, opts, {
+        particleCount: Math.floor(count * particleRatio),
+      })
+    );
+  };
 
-	const handleClose = () => {
-		setOpen(false);
-	};
+  const [open, setOpen] = useState(false);
 
-	const navigate = useNavigate();
+  const handleOpen = () => {
+    fire(0.25, {
+      spread: 26,
+      startVelocity: 55,
+    });
+    fire(0.2, {
+      spread: 60,
+    });
+    fire(0.35, {
+      spread: 100,
+      decay: 0.91,
+      scalar: 0.8,
+    });
+    fire(0.1, {
+      spread: 120,
+      startVelocity: 25,
+      decay: 0.92,
+      scalar: 1.2,
+    });
+    fire(0.1, {
+      spread: 120,
+      startVelocity: 45,
+    });
 
-	const { user } = useAuth();
+    setOpen(true);
+  };
 
-	const isMobile = useMediaQuery(MOBILE);
+  const handleClose = () => {
+    setOpen(false);
+  };
 
-	const isParent = () => {
-		if (user.role === 'parent') {
-			return true;
-		}
-	};
+  const navigate = useNavigate();
 
-	return (
-		<Stack sx={{ width: '100%' }}>
-			<Typography
-				className="headingFont"
-				variant="subtitle1"
-				gutterBottom
-				align="center">
-				Certificates:
-			</Typography>
-			{childData?.certificates?.length === 0 && (
-				<>
-					{isParent() === false ? (
-						<>
-							<Alert variant="outlined" severity="info">
-								{childData.firstName} {childData.lastName} has no certificate
-								yet, click on the 'Create Certificate' button to submit one.
-							</Alert>
-							<Button
-								sx={{ mt: 2, width: '100%' }}
-								variant="contained"
-								color="warning"
-								size="small"
-								onClick={() => {
-									navigate('/certificate/new', { replace: true });
-								}}>
-								Create Certificate
-							</Button>
-						</>
-					) : (
-						<>
-							<Alert variant="outlined" severity="info">
-								{childData.firstName} {childData.lastName} has no certificate
-								yet.
-							</Alert>
-						</>
-					)}
-				</>
-			)}
-			<Stack
-				flexDirection="row"
-				flexWrap="wrap"
-				justifyContent="space-around"
-				sx={{ width: '100%' }}>
-				{childData?.certificates?.map((certificate, index) => {
-					if (index + 1 === childData.certificates.length) {
-						return (
-							<ChildCertificateCard
-								key={index}
-								backgroundImage={certificateImg(certificate.certificateType)}
-								studentName={certificate.name}
-								message={certificate.message}
-							/>
-						);
-					} else {
-						return <></>;
-					}
-				})}
-			</Stack>
-			{childData?.certificates?.length === 0 ? (
-				<></>
-			) : (
-				<Button onClick={handleOpen}>See all Certificates</Button>
-			)}
-			<Modal
-				open={open}
-				onClose={handleClose}
-				aria-labelledby="modal-modal-title"
-				aria-describedby="modal-modal-description">
-				<Box
-					sx={{
-						...modal.container,
-						width: isMobile ? '60%' : '80%',
-						display: 'flex',
-						flexWrap: 'wrap',
-						justifyContent: 'space-evenly',
-					}}>
-					{childData?.certificates?.map((certificate, index) => {
-						return (
-							<ChildCertificateCard
-								key={index}
-								backgroundImage={certificateImg(certificate.certificateType)}
-								studentName={certificate.name}
-								message={certificate.message}
-							/>
-						);
-					})}
-				</Box>
-			</Modal>
-		</Stack>
-	);
+  const { user } = useAuth();
+
+  const isMobile = useMediaQuery(MOBILE);
+
+  const isParent = () => {
+    if (user.role === "parent") {
+      return true;
+    }
+  };
+
+  return (
+    <Stack sx={{ width: "100%" }}>
+      <Typography
+        className="headingFont"
+        variant="subtitle1"
+        gutterBottom
+        align="center"
+      >
+        Certificates:
+      </Typography>
+      {childData?.certificates?.length === 0 && (
+        <>
+          {isParent() === false ? (
+            <>
+              <Alert variant="outlined" severity="info">
+                {childData.firstName} {childData.lastName} has no certificate
+                yet, click on the 'Create Certificate' button to submit one.
+              </Alert>
+              <Button
+                sx={{ mt: 2, width: "100%" }}
+                variant="contained"
+                color="warning"
+                size="small"
+                onClick={() => {
+                  navigate("/certificate/new", { replace: true });
+                }}
+              >
+                Create Certificate
+              </Button>
+            </>
+          ) : (
+            <>
+              <Alert variant="outlined" severity="info">
+                {childData.firstName} {childData.lastName} has no certificate
+                yet.
+              </Alert>
+            </>
+          )}
+        </>
+      )}
+      <Stack
+        flexDirection="row"
+        flexWrap="wrap"
+        justifyContent="space-around"
+        sx={{ width: "100%" }}
+      >
+        {childData?.certificates?.map((certificate, index) => {
+          if (index + 1 === childData.certificates.length) {
+            return (
+              <ChildCertificateCard
+                key={index}
+                backgroundImage={certificateImg(certificate.certificateType)}
+                studentName={certificate.name}
+                message={certificate.message}
+              />
+            );
+          } else {
+            return <></>;
+          }
+        })}
+      </Stack>
+      {childData?.certificates?.length === 0 ? (
+        <></>
+      ) : (
+        <Button
+          sx={{ mt: 2, width: "100%" }}
+          variant="contained"
+          color="warning"
+          size="small"
+          onClick={handleOpen}
+        >
+          See all Certificates
+        </Button>
+      )}
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box
+          sx={{
+            ...modal.container,
+            width: isMobile ? "60%" : "80%",
+            display: "flex",
+            flexWrap: "wrap",
+            justifyContent: "space-evenly",
+          }}
+        >
+          {childData?.certificates?.map((certificate, index) => {
+            return (
+              <ChildCertificateCard
+                key={index}
+                backgroundImage={certificateImg(certificate.certificateType)}
+                studentName={certificate.name}
+                message={certificate.message}
+              />
+            );
+          })}
+        </Box>
+      </Modal>
+    </Stack>
+  );
 };
