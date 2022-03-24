f41251ae7e20219a6fedf2859c3fa83fd7db4995
Luiz Carlos Froes <luizfroes@gmail.com>
2022-03-24 00:14:14 +0000

    Added certificates modal

--- src/media.js
+++ src/media.js
@@ -2,4 +2,4 @@ export const MOBILE = { query: "(max-width: 650px)" };
 
 export const DESKTOP = { query: "(min-width: 651px)" };
 
-export const TABLET = { query: "(min-width: 900px)" };
+export const TABLET = { query: "(min-width: 900px, max-width: 1199)" };
